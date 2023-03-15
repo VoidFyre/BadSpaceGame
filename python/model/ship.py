337f1d3de294aaf76aa0a693d6cc49db9cef3f35
Damyon <damyonolson1@gmail.com>
2023-03-15 15:13:34 -0400

    Updated ship.py
    Began working with adding a shooting function so that when the player shoots the bullets leave the ship and project towards the enemy.

--- python/model/ship.py
+++ python/model/ship.py
@@ -4,6 +4,8 @@ class Ship(pygame.sprite.Sprite):
     def __init__(self, equipped, inventory, posx, posy):
         super().__init__()
 
+        self.posx = posx
+        self.posy = posy
         self.base_health = 300
         self.base_armor = 100
         self.base_shield = 100
@@ -11,3 +13,17 @@ class Ship(pygame.sprite.Sprite):
         self.inventory = [None, None, None, None, None, None]
 
         self.image = pygame.image.load("./assets/ship_base.png")
+
+    def shootPrimary(self):
+        color = {0,0,0}
+        self.primary = self.equipped['primary']
+        #self.projectile = #projectile image
+
+        self.projectileVel = {0,1}
+
+        projectile = pygame.draw.rect(self.primary.image, color, pygame.Rect(self.posx, self.posy, self.primary.x, self.primary.y))
+
+        projectile.colliderect()
+
+
+
