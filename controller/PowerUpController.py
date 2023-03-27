e68de780b99eb52fcd8dedc70c0d97cc995847df
Damyon <damyonolson1@gmail.com>
2023-03-27 15:27:44 -0400

    Updated Controllers
    Altered the controllers to have a better setup

---
+++ controller/PowerUpController.py
@@ -0,0 +1,6 @@
+import pygame
+
+class PowerUpController:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
---
+++ controller/SpaceshipController.py
@@ -0,0 +1,6 @@
+import pygame
+
+class SpaceshipController:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
